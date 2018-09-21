1de67c0548a15c82ec38a7a7b6c111656a986a9b
Sagar <sagarthomas27@hotmail.com>
2018-09-20 20:13:48 -0400

    create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='table-maker',
+      version='0.1.0',
+      description='A library which assists in generating tables in Python',
+      url='https://github.com/Matthew-Dombrady/table-maker',
+      author='Matthew Dombrady and Sagar Thomas',
+      license='MIT',
+      packages=['table-maker'],
+      zip_safe=False)
