b91683312912e7ea7487229ec37ccd7ddcf8d794
Matthew-Dombrady <matthew.dombrady@gmail.com>
2018-09-22 18:36:14 -0400

    create addEntry() and addEntries() functions

--- sandbox/test.py
+++ sandbox/test.py
@@ -1,6 +1,11 @@
 from table_maker import Table
 
-t = Table.Table('Test table', ['Student name', 'GPA'], '#')
+t = Table.Table('Test table', ['Student name', 'GPA', "Jobs", "GFs"], '#')
+t.addEntry(["Ardhendu", "12", "20", "100"])
+t.addEntry(["Sagar", "12", "6", "5"])
+t.addEntry(["Matthew", "1", "0", "0"])
+t.addEntry(["Giannis Antentekounpo", "12", "1", "1"])
+t.addEntries([["Venushan", "5", "0", "-1"],["John", "12", "0", "0"]])
 t.print()
 
 
--- table_maker/Table.py
+++ table_maker/Table.py
@@ -5,6 +5,7 @@ class Table:
         self.labels = labels
         self.symbol = symbol
         self.max_lengths = []
+        self.data = []
 
         for label in labels:
             self.max_lengths.append(len(label))
@@ -12,14 +13,33 @@ class Table:
     
 
     def print(self):
-        for label in self.labels:
-            print(label + "  ", end="")
+        for i in range(len(self.labels)):
+            print(self.labels[i] + "  " + " "*(self.max_lengths[i]-len(self.labels[i])), end="")
             self.__symbol__(1)
         print("\n")
-        self.__symbol__((sum(self.max_lengths)) + (4 * len(self.labels)))
+        self.__symbol__((sum(self.max_lengths)) + (4 * len(self.labels)) + 2)
+        print("\n")
+        
+        for entry in self.data:
+            for i in range(len(entry)):
+                print(entry[i] +"  "+" "*(self.max_lengths[i]-len(entry[i])), end="")
+                self.__symbol__(1)
+            print("\n")
+
+    def addEntry(self, x):
+        self.data.append(x)
+
+        for i in range(len(x)):
+            if(len(x[i]) > self.max_lengths[i]):
+                self.max_lengths[i] = len(x[i])
+
+    def addEntries(self, x):
+        for entry in x:
+            self.addEntry(entry)
         
 
     def __symbol__(self, num):
         print(self.symbol * num + "  ", end="")
+
 
     
